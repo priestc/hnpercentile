7da84f86143bbd295e0537537ef97bdcb4a61c11
Chris Priest <nbvfour@gmail.com>
2013-01-24 16:01:37 -0500

    fixed migration

--- db/migrate/20130124181113_get_date_reg_data.rb
+++ db/migrate/20130124181113_get_date_reg_data.rb
@@ -1,7 +1,7 @@
 class GetDateRegData < ActiveRecord::Migration
   def up
     Member.all.each do |member|
-      url = "http://api.thriftdb.com/api.hnsearch.com/users/" + username
+      url = "http://api.thriftdb.com/api.hnsearch.com/users/" + member.username
       doc = open(url).read
       j = JSON.parse(doc)
       member.date_registered =j['created_ts'].to_date
