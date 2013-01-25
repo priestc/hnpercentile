38f7056dc9c16be126ce6eceea027992e36d8b02
Chris Priest <nbvfour@gmail.com>
2013-01-24 23:45:09 -0500

    fixed migration so it actually saves

--- db/migrate/20130124181113_get_date_reg_data.rb
+++ db/migrate/20130124181113_get_date_reg_data.rb
@@ -1,10 +1,11 @@
 class GetDateRegData < ActiveRecord::Migration
   def up
-    Member.all.each do |member|
+    Member.where(:date_registered => nil).each do |member|
       url = "http://api.thriftdb.com/api.hnsearch.com/users/" + member.username
       doc = open(url).read
       j = JSON.parse(doc)
       member.date_registered =j['create_ts'].to_date
+      member.save
       sleep 1.0
     end
     nil
