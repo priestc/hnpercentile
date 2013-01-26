e3c0e3dd3019b620bc8aefc2a9035f43f3619b05
Chris Priest <nbvfour@gmail.com>
2013-01-25 21:44:26 -0500

    made username box a form

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def show
-    @member = Member.where(:username => params[:username]).first
+    @member = Member.where(:username => params[:username]).first!
     if not @member
       @member = Member.make_from_api(params[:username])
     else
@@ -9,7 +9,7 @@ class IndexController < ApplicationController
     attrs = @member.attributes
     attrs[:percentile] = @member.percentile
     attrs[:month_percentile] = @member.percentile(:date=>true)
-    
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: attrs }
