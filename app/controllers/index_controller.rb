25c82f3054bde7a9bb4e5981d64c8dcdf624cec4
Chris Priest <nbvfour@gmail.com>
2013-01-30 16:40:50 -0500

    added links to navigate between months

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -23,8 +23,16 @@ class IndexController < ApplicationController
     end_date = start_date.end_of_month
     @members = Member.where(:date_registered => start_date..end_date).order("karma DESC")
     @max_karma = @members.first.karma
-    @percent_of_total_by_users = @members.count / Member.count.to_f
-    @percent_of_total_by_karma = @members.sum(:karma) / Member.sum(:karma).to_f
+    @percent_of_total_by_users = @members.count / Member.count.to_f * 100
+    @percent_of_total_by_karma = @members.sum(:karma) / Member.sum(:karma).to_f * 100
+    
+    @next_month_obj = (end_date + 15.days).beginning_of_month
+    @next_month = @next_month_obj.strftime("%Y %B")
+    @next_month_link = "/month/#@next_month".sub(' ', '-').downcase
+    
+    @prev_month_obj = (start_date - 15.days).beginning_of_month
+    @prev_month = @prev_month_obj.strftime("%Y %B")
+    @prev_month_link = "/month/#@prev_month".sub(' ', '-').downcase
     
     respond_to do |format|
       format.html # show.html.erb
